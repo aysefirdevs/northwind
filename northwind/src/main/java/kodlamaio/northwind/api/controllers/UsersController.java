c5f11d40039fe9a2cdfb2a28f3001e1f9df4a3af
Ayşe Firdevs Çelik <50719300+aysefirdevs@users.noreply.github.com>
2021-05-30 23:47:10 +0300

    added user

---
+++ northwind/src/main/java/kodlamaio/northwind/api/controllers/UsersController.java
@@ -0,0 +1,32 @@
+package kodlamaio.northwind.api.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.northwind.business.abstracts.UserService;
+import kodlamaio.northwind.core.entities.User;
+import kodlamaio.northwind.core.utilities.results.Result;
+
+@RestController
+@RequestMapping(value="/api/users")
+public class UsersController {
+	
+	private UserService userService;
+	
+	@Autowired
+	public UsersController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+	
+	@PostMapping(value="/add")
+	public ResponseEntity<?> add(@RequestBody User user) { 	
+		return ResponseEntity.ok(this.userService.add(user));
+	}
+	
+	
+}
